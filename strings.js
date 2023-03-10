15f519d75c802a23566b35cec6756914a84729ed
Mitingi Joy <mitingijoychesang@gmail.com>
2023-03-10 20:08:22 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,35 @@
+//Extract the first four characters from the string below;
+"beautiful"
+var x = "beautiful"
+console.log(x[0]);
+console.log(x[1]);
+console.log(x[2]);
+console.log(x[3]);
+//Insert the following string at the tenth index of the below variable:
+"eat"
+const food = "I did not have appetite today"
+v = "eat"
+const food2 = "I did not have appetite${v} today"
+//Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."s"
+const story= "She sells sea shells at the sea shore"
+let count = (story.match(/the/)).length
+console.log(count);
+let count2 = (story.match(s)).length
+console.log(count2);
+//Convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
+const y = "coNfidant"
+const x = y.toUpperCase()
+console.log(x)
+const v = "amazing"
+const u = v.toLowerCase()
+console.log(u)
+const w = "beautiful"
+const p = w.toLowerCase()
+console.log(p)
+
+
